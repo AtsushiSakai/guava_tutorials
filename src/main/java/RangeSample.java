d4a4041c8d7980d883f3216fc2afc9497f77b3a5
Atsushi Sakai <asakai.amsl+github@gmail.com>
2020-12-27 14:07:33 +0900

    Add time range sample

--- src/main/java/RangeSample.java
+++ src/main/java/RangeSample.java
@@ -1,5 +1,7 @@
 import com.google.common.collect.Range;
 
+import java.time.LocalDate;
+
 public class RangeSample {
     public static void main(String[] args) {
         // Int range
@@ -13,6 +15,15 @@ public class RangeSample {
 
         // gap range
         System.out.println(range2.gap(Range.closed(-1.0, 0.0))); // (0.0..1.0)
+
+        //Time range sample
+        LocalDate date1 = LocalDate.of(2013, 12, 31);
+        LocalDate date2 = LocalDate.of(2014, 1, 10);
+        Range<LocalDate> timeRange = Range.closedOpen(date1, date2);
+        System.out.println(timeRange);
+
+        System.out.println(timeRange.contains(LocalDate.of(2014, 1, 1))); // true
+        System.out.println(timeRange.contains(LocalDate.of(2014, 1, 11)));// false
 
     }
 }
