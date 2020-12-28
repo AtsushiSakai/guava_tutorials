fc8f7e66ec369260b1b707472b48279c55938737
Atsushi Sakai <asakai.amsl+github@gmail.com>
2020-12-28 22:28:56 +0900

    Add StopWatchSample

---
+++ src/main/java/StopWatchSample.java
@@ -0,0 +1,20 @@
+import com.google.common.base.Stopwatch;
+
+import java.util.concurrent.TimeUnit;
+
+public class StopWatchSample {
+    public static void main(String[] args) throws InterruptedException {
+        Stopwatch sw = Stopwatch.createStarted();
+
+        while(true) {
+            System.out.println(sw.elapsed(TimeUnit.SECONDS) + "sec");
+            Thread.sleep(5000);
+            if (sw.elapsed(TimeUnit.SECONDS) > 30) {
+                sw.stop();
+                sw.reset();
+                System.out.println("Done");
+                break;
+            }
+        }
+    }
+}
