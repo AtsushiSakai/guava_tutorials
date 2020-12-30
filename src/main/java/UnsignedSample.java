739bd13d195d7f2f1f141a7982f161e054688a48
Atsushi Sakai <asakai.amsl+github@gmail.com>
2020-12-30 21:06:43 +0900

    Add UnsignedSample

---
+++ src/main/java/UnsignedSample.java
@@ -0,0 +1,23 @@
+import com.google.common.primitives.UnsignedInteger;
+import com.google.common.primitives.UnsignedInts;
+
+public class UnsignedSample {
+
+    public static void main(String[] args) {
+        int a = 0;
+        a-=1;
+        System.out.println(a);// -1
+
+        UnsignedInteger ua = UnsignedInteger.valueOf(1);
+        ua = ua.minus(UnsignedInteger.valueOf(1));
+        System.out.println(ua);// 0
+        ua = ua.minus(UnsignedInteger.valueOf(1));
+        System.out.println(ua);// 4294967295 (underflow)
+
+        a = Integer.MAX_VALUE;
+        System.out.println(a);// 2147483647
+        a+=1;
+        System.out.println(a);//-2147483648 (overflow)
+
+    }
+}
